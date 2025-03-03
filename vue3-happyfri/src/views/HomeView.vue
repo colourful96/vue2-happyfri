537d2f9f7827cfe7a3c5948a6d6c4b56686f28eb
colourful <colourful4570@163.com>
2025-03-03 17:47:00 +0800

    使用vue3实现

---
+++ vue3-happyfri/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ItemContainer from '@/components/ItemContainer.vue'
+</script>
+
+<template>
+  <main>
+    <ItemContainer :fatherComponent="'home'"></ItemContainer>
+  </main>
+</template>
---
+++ vue3-happyfri/src/views/ItemView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onBeforeMount } from 'vue'
+import ItemContainer from '@/components/ItemContainer.vue'
+import useGlobalStore from '@/stores/index.js'
+
+const globalStore = useGlobalStore()
+
+onBeforeMount(() => {
+  globalStore.rememberTime()
+})
+</script>
+
+<template>
+  <ItemContainer father-component="item"></ItemContainer>
+</template>
+
+<style scoped lang="less"></style>
